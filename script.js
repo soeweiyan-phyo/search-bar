0e78411a2ec98a6db267848dc0fb7ada81582293
Soe Wei Yan Phyo <soeweiyan.phyo>
2023-07-25 11:02:08 +1000

    Add auto focus for seach input

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const searchInputWrapper = document.querySelector(".search-input-wrapper");
 const searchIcon = document.querySelector(".search-icon i");
+const searchInput = document.querySelector(".search-input input");
 const closeIcon = document.querySelector(".search-input i");
 
-if (searchIcon && searchInputWrapper) {
+if (searchIcon && searchInputWrapper && searchInput) {
   searchIcon.addEventListener("click", () => {
     searchIcon.parentElement.classList.add("change");
     searchInputWrapper.classList.add("change");
+
+    setTimeout(() => {
+      searchInput.focus();
+    }, 1000);
   });
 }
 
